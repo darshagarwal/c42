b5e547d567728abc86a257eea239465e5ac77f2c
shirishagajam <64522203+shirishagajam@users.noreply.github.com>
2020-08-07 16:25:45 +0530

    Update sketch.js
    Just placed some code in draw and changed the coordinates of line

--- sketch.js
+++ sketch.js
@@ -3,31 +3,36 @@ var minu,minuAngle;
 var sc,scAngle;
 
 function setup() {
-  createCanvas(800,400);
+   createCanvas(400,400);
+  angleMode(DEGREES);
 
+ 
+}
+
+function draw() {
+  background(255,255,255);  
+  
+  translate(200,200)
+  rotate(-90)
+  
   hours = hour();
   minu = minute();
   sc = second();
 
   scAngle = map(sc,0,60,0,360);
   hoursAngle = map(hours,0,60,0,360);
-}
-
-function draw() {
-  background(255,255,255);  
-  angleMode(DEGREES);
 
   push();
   rotate(scAngle);
   stroke(255,0,0);
   strokeWeight(7);
-  line(50,200,300,100);
+  line(0,0,100,0);
   pop();
 
-  push();
+  /*push();
   rotate(hoursAngle);
   stroke("green");
   strokeWeight(7);
   line(50,200,300,100);
-  pop();
+  pop();*/
 }
